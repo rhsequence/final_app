6abbf0d8d8a9d685fa52e103df47a88205a9f767
Richard Hernandez <rhsequence@gmail.com>
2011-12-08 14:35:42 -0600

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 FinalApp::Application.routes.draw do
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => 'pages#home'
   match '/roster', :to => 'pages#roster'  
@@ -9,6 +12,9 @@ FinalApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
 
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
