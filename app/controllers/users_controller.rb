570342df421c214328f6045c7e0e84146dca1bbf
Richard Hernandez <rhsequence@gmail.com>
2011-12-08 13:57:40 -0600

    finished sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,21 @@ class UsersController < ApplicationController
 
   def show
 	@user = User.find(params[:id])
+	@title = @user.name
   end
 
   def new
+	@user = User.new
 	@title = "Sign Up"
   end
 
+  def create
+	@user = User.new(params[:user])
+	if @user.save
+		redirect_to @user
+	else
+	   @title = "Sign Up"
+	   render 'new'
+	end
+  end
 end
